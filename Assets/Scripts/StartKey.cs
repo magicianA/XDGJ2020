5873a9c5c45739cb042b1e8672aded3e105faaf8
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2020-12-27 14:08:03 +0800

    StartScene

---
+++ Assets/Scripts/StartKey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartKey : MonoBehaviour
+{
+
+    public void getKeyDownCode()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("GameScene");
+            
+        }
+        
+    }
+
+    void Update()
+    {
+        getKeyDownCode();
+    }
+}
